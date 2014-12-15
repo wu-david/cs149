473562e74d6abf5580cd4df41e78c2679ab66ede
Harrison Tsai <harrisontsai0123@gmail.com>
2014-12-14 21:55:51 -0800

    added accelerometer

--- servoLock/servoLock.ino
+++ servoLock/servoLock.ino
@@ -1,6 +1,10 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 #include <IRremote.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_9DOF.h>
 
 /* pin definitions
  * 3 - IR TX
@@ -9,6 +13,8 @@
  * 9 - Servo
  * 11 - IR RX
  * 13 - LED
+ * A4 - Accelerometer SDA
+ * A5 - Accelerometer SCL
  */
 #define IRTXPIN 3
 #define BTRXPIN 5
@@ -17,13 +23,14 @@
 #define IRRXPIN 11
 #define LEDPIN 13
 
-
 /* declarations */
 Servo lockServo;
 SoftwareSerial bluetooth(BTTXPIN, BTRXPIN);
+SoftwareSerial accelerometer(A4, A5);
 String dataFromBT;
 IRsend irsend;
- 
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+
 void setup() {
   /* servo setup */
   lockServo.attach(SERVOPIN);
@@ -42,9 +49,23 @@ void setup() {
   pinMode(LEDPIN, OUTPUT);
   irsend.enableIROut(38);
   irsend.mark(0);
+  
+  /* accelerometer setup */
+  accelerometer.begin(57600);
+  if(!accel.begin()) {
+    Serial.println(F("LSM303 not detected"));
+    while(1);
+  }
 }
  
 void loop() {
+  sensors_event_t event;
+  
+  accel.getEvent(&event);
+  Serial.println(event.acceleration.x);
+  Serial.println(event.acceleration.y);
+  Serial.println(event.acceleration.z);
+  
   digitalWrite(LEDPIN, !digitalRead(IRRXPIN));
   while (bluetooth.available()) {
     char received = bluetooth.read();
@@ -67,5 +88,6 @@ void loop() {
         dataFromBT = "";
       }
     }
-  }  
+  }
+  delay(50);  
 }
